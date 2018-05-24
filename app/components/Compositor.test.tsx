c5146ffcdc4028cee416ae6eeb0cb8149d08cc5d
Todd Medema <toddmedema@gmail.com>
2018-05-23 20:44:33 -0400

    cleanup

--- app/components/Compositor.test.tsx
+++ app/components/Compositor.test.tsx
@@ -1,5 +1,3 @@
-describe('Main', () => {
-  it('does not trigger animation if state update to same card');
-
-  it('intercepts and publishes touch/move events');
+describe('Compositor', () => {
+  it('Renders the provided card');
 });
