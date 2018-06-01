c68083b398d3b953076f10cd4771d967633aa747
Eric Zittel <ezittel@nycap.rr.com>
2018-06-01 15:38:06 -0400

    Please review, incorporates all requested changes.

--- app/components/views/Search.tsx
+++ app/components/views/Search.tsx
@@ -257,17 +257,14 @@ export function renderResult(props: SearchResultProps): JSX.Element {
           <tbody>
             <tr>
               <th className="leftcell">
-                <div className="title">
                   <Truncate lines={2}>
                     {quest.title}
                   </Truncate>
-                </div>
               </th>
               <th className="rightcell">
-                <div className="indicators">{/*props.lastPlayed &&*/ <DoneIcon className="inline_icon" />}
-                  {/*quest.official !== undefined && quest.official &&*/ <span className="indicator_spacer"><img className="inline_icon" src="images/compass_small.svg"/></span>}
-                  {/*quest.awarded &&*/ <StarsIcon className="inline_icon" />}
-                </div>
+                {props.lastPlayed && <DoneIcon className="inline_icon" />}
+                {quest.official !== undefined && quest.official && <span className="indicator_spacer"><img className="inline_icon" src="images/compass_small.svg"/></span>}
+                {quest.awarded && <StarsIcon className="inline_icon" />}
               </th>
             </tr>
           </tbody>
